68cfc334a7c888c8995fb575ef9371b24bb0a3d8
Anja Hagedorn <33956722+dranjahagedorn@users.noreply.github.com>
2026-05-02 12:56:36 +0200

    setup

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import GartenApp from './garten-app-supabase.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <GartenApp />
+  </React.StrictMode>
+)
